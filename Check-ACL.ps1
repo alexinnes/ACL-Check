07be58c27c1aae9db6c77a6d76544002a3fc205c
Alex Innes <alexnixoninnes@gmail.com>
2019-03-20 10:07:18 +0000

    Made this into a function for ease of use

--- Check-ACL.ps1
+++ Check-ACL.ps1
@@ -1,22 +1,34 @@
-$sw = [Diagnostics.Stopwatch]::StartNew()
-$filePath = "C:\"
+function Get-FolderPermissions {
+    [CmdletBinding()]
+    param (
+    # Parameter help description
+    [Parameter(Mandatory=$true)]
+    $filePath = "C:\",
 
-$folders = [System.IO.Directory]::EnumerateDirectories($filePath,"*",1)
-$folderAcl = @()
-foreach($f in $folders){
-    $acl = Get-Acl -Path $f
-   $folderAcl += New-Object pscustomobject -Property @{
-        path = $f
-        FileSystemRights = "$($acl.Access.FileSystemRights)"
-        AccessControlType = "$($acl.Access.AccessControlType)"
-        IdentityReference = "$($acl.Access.IdentityReference)"
-        Inherited = "$($acl.Access.IsInherited)"
+    # Parameter help description
+    [Parameter(Mandatory=$true)]
+    $outputCSV = "C:\temp\PermissionOutput.csv"
 
+    )
+
+    $folders = [System.IO.Directory]::EnumerateDirectories($filePath,"*",1)
+    $folderAcl = @()
+    foreach($f in $folders){
+        $acl = Get-Acl -Path $f
+       $folderAcl += New-Object pscustomobject -Property @{
+            path = $f
+            FileSystemRights = "$($acl.Access.FileSystemRights)"
+            AccessControlType = "$($acl.Access.AccessControlType)"
+            IdentityReference = "$($acl.Access.IdentityReference)"
+            Inherited = "$($acl.Access.IsInherited)"
+
+
+        }
 
     }
+    $folderAcl | Export-Csv $outputCSV
+
 
 }
-$folderAcl | Export-Csv C:\temp\tem.csv
 
-$sw.Stop()
-$sw.Elapsed
+
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,2 @@
 # Check Folder ACL
 Enumerates all folders and lists permissions on them. (quick and dirty)
-It also outputs how long it took.
